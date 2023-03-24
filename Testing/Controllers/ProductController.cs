9c1809bc5e5467e78a354695d29c53a6de691dee
Maxwell Largegit config --global user.email Maxwell.Large@gmail.comgit config --global user.name Maxwell <maxwell.large@gmail.com>
2023-03-24 17:38:38 -0400

    fixed code

---
+++ Testing/Controllers/ProductController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Testing.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IProductRepository _repository;
+
+        public ProductController(IProductRepository repo)
+        {
+            _repository = repo;
+        }
+
+
+        public IActionResult Index()
+        {
+            var products = _repository.GetAllProducts();
+
+            return View(products);
+        }
+    }
+}
---
+++ Testing/ProductRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Testing.Models;
+using Dapper;
+using System.Data;
+namespace Testing
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly IDbConnection _connection;
+
+        public ProductRepository(IDbConnection conn)
+        {
+            _connection = conn;
+        }
+
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return _connection.Query<Product>("SELECT * FROM products;");
+        }
+    }
+}
